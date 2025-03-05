34427519268b01841bdc458e08edea7096f8c204
EkstraPC <EkstraPC@DESKTOP-NODCD56>
2025-03-05 18:31:07 +0100

    Mit meeega gode spil

--- CSharpBegynderOpgave_3/Program.cs
+++ CSharpBegynderOpgave_3/Program.cs
@@ -1,18 +1,28 @@
 ﻿//Lav et spil hvor man kan gætte et, af computeren, random genereret tal.
 
 
+
+int Midte = 40;
+
+Console.ForegroundColor = ConsoleColor.White;
+
+
 //En Console.ReadLine() returnere altid en streng, hvis vi skal lave den om til en int er vi nødt til at pakke det ind i Convert.ToInt()
-Console.WriteLine("Indtast et tal:");
+Console.SetCursorPosition(Midte, 7);
+Console.WriteLine("Hvad er dit gæt?:");
+
+Console.SetCursorPosition(Midte, 8);
 int input = Convert.ToInt32(Console.ReadLine()); //Det giver altså en fejl hvis vi taster noget ind som IKKE er et tal!
-Console.WriteLine(input); //Udskriv inputtet.
+
+
 
 
 
 
 
 Random random = new Random();
-int Resultat = random.Next(0,11); //Generere et random tal imellem 0 og 100. (0 er med, men 101 er ikke)
-Console.WriteLine("hvad er dit gæt?");
+int Resultat = random.Next(1,11); //Generere et random tal imellem 0 og 100. (0 er med, men 101 er ikke)
+
 
 
 
@@ -21,16 +31,24 @@ Console.WriteLine("hvad er dit gæt?");
 
 //Dette er if statements og bruges til at sammenligne værdier, det skal bruges til spillet.
 if (input > Resultat)
+
 {
+	Console.SetCursorPosition(Midte, 9);
+    Console.ForegroundColor = ConsoleColor.Red;
 	Console.WriteLine(" hahaha Du svarede forkert");
 }
 else if (input < Resultat)
+
 {
+    Console.SetCursorPosition(Midte, 9);
+    Console.ForegroundColor = ConsoleColor.Red;
 	Console.WriteLine("hahaha Du svarede forkert");
 }
 else 
 {
-	Console.WriteLine("Du vandt");
+    Console.SetCursorPosition(Midte, 9);
+    Console.ForegroundColor = ConsoleColor.Green;
+	Console.WriteLine(" YAY Du vandt!!!");
 }
 
 
